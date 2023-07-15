63c87287e11033f971f0b84ca96228a348ecfc79
yuanching.chen.appledev@gmail.com <yuanching.chen.appledev@gmail.com>
2023-07-15 16:13:55 +0800

    Initial Commit

---
+++ Insta/InstaApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaApp.swift
+//  Insta
+//
+//  Created by YuanChingChen on 2023/7/15.
+//
+
+import SwiftUI
+
+@main
+struct InstaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
